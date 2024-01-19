0ed19cc2538872ab7bec13b261376ed6bedaae89
fadelyking <fadely195@gmail.com>
2024-01-19 10:45:04 +0300

    setup listings schema

---
+++ models/listing.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+
+const ListingSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  likes: Number,
+  comments: [
+    {
+      user: { type: Schema.Types.ObjectId, ref: "User" },
+      text: { type: String, required: true },
+      likes: Number,
+      dislikes: Number,
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+  urgency: Number,
+  views: Number,
+  createdAt: { type: Date, default: Date.now },
+});
+
+ListingSchema.virtual("url").get(function () {
+  return "/" + this._id;
+});
+
+module.exports = mongoose.model("Listing", ListingSchema);
