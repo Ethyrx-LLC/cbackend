8c16c54a89076721498c92a6c220c58b9c5b5452
fadelyking <fadely195@gmail.com>
2024-01-20 11:57:06 +0300

    added verification

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,12 +1,13 @@
+require("dotenv").config;
 const Listings = require("../models/listing");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
-
+const KEY = process.env.SECRET_TOKEN;
 // Returns an array of listings
 exports.display_listings_all = asyncHandler(async (req, res, next) => {
   const listings = await Listings.find().populate("user").exec();
-  res.json(listings);
+  res.json({ success: true, listings: listings });
 });
 
 // Returns a specific listing based on ID
@@ -18,11 +19,19 @@ exports.display_listing_detail = asyncHandler(async (req, res, next) => {
   if (listing === null) {
     res.status(404).json("Page not Found");
   } else {
-    res.json(listing);
+    res.json({ success: true, listing: listing });
   }
 });
 
-exports.create_listing_get = (req, res, next) => {};
+exports.create_listing_get = (req, res, next) => {
+  jwt.verify(req.cookies.token, KEY, (err, authData) => {
+    if (err) {
+      res.json({ success: false, message: "User not logged in" });
+    } else {
+      res.json({ success: true, authData });
+    }
+  });
+};
 
 exports.create_listing_post = [
   body("title", "Title must be more than 3 characters long")
@@ -36,44 +45,60 @@ exports.create_listing_post = [
   body("category", "Please select a category").notEmpty(),
 
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
+    jwt.verify(req.cookies.token, KEY, (err, authData) => {
+      if (err) {
+        res.status(401).json({ success: false, message: "Unauthorized" });
+      } else {
+      }
+      const errors = validationResult(req);
 
-    const listing = new Listings({
-      user: authData.user.username,
-      title: req.body.title,
-      content: req.body.content,
-      category: req.body.category,
-      urgency: req.body.urgency || 0,
+      const listing = new Listings({
+        user: authData.user.username,
+        title: req.body.title,
+        content: req.body.content,
+        category: req.body.category,
+        urgency: req.body.urgency || 0,
+      });
+
+      if (!errors.isEmpty()) {
+        res.status(401).json({ success: false, error: errors.array() });
+      } else {
+        listing.save();
+        res.status(200).json({ success: true });
+      }
     });
-
-    if (!errors.isEmpty()) {
-      res.status(401).json({ success: false, error: errors.array() });
-    } else {
-      listing.save();
-      res.status(200).json({ success: true });
-    }
   }),
 ];
 
 exports.delete_listing_post = asyncHandler(async (req, res, next) => {
-  const listing = await Listings.findById(req.params.id).exec();
-
-  if (listing === null) {
-    res.json({ success: false, error: "No post found" });
-  } else {
-    await listing.deleteOne();
-    res.json({ success: true, error: "Post deleted" });
-  }
+  const listing = Listings.findById(req.params.id).exec();
+  jwt.verify(req.cookies.token, KEY, (err, authData) => {
+    if (err) {
+      res.status(401).json({ success: false, message: "Unauthorized" });
+    } else {
+      if (listing === null) {
+        res.json({ success: false, error: "No post found" });
+      } else {
+        listing.deleteOne();
+        res.json({ success: true, error: "Post deleted" });
+      }
+    }
+  });
 });
 
 exports.upvote_listing_post = asyncHandler(async (req, res, next) => {
   const listing = await Listings.findById(req.params.id);
-
-  if (listing === null) {
-    res.json({ success: false, error: "No post found" });
-  } else {
-    listing.likes += 1;
-    listing.save();
-    res.json({ success: true, likes: listing.likes });
-  }
+  jwt.verify(req.cookies.token, KEY, (err, authData) => {
+    if (err) {
+      res.status(401).json({ success: false, message: "Unauthorized" });
+    } else {
+      if (listing === null) {
+        res.json({ success: false, error: "No post found" });
+      } else {
+        listing.likes += 1;
+        listing.save();
+        res.json({ success: true, likes: listing.likes });
+      }
+    }
+  });
 });
