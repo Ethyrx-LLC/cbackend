615d0ef70b95e3b5267427ce7584f90d73f05f8f
fadelyking <fadely195@gmail.com>
2024-01-20 11:57:14 +0300

    added routes and authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
 const listing_controller = require("../controllers/listing");
+const user_controller = require("../controllers/user");
+const authorize = require("../middleware/verification");
 
 /* GET home page. */
 
-router.get("/listings", listing_controller.display_all_listings);
+router.get("/listings/create", authorize, listing_controller.create_listing_get);
+router.post("/listings/create", authorize, listing_controller.create_listing_post);
+router.get("/listings", listing_controller.display_listings_all);
 router.get("/listing-detail/:id", listing_controller.display_listing_detail);
+router.delete("/listing-detail/:id/delete", authorize, listing_controller.delete_listing_post);
+router.put("/listing-detail/:id/upvote", authorize, listing_controller.upvote_listing_post);
+
+router.post("/user/create", user_controller.create_users_post);
+router.post("/user/login", user_controller.create_users_post);
+router.post("/user/logout", user_controller.logout_post);
+router.get("/users", user_controller.users_get);
+router.get("/user/:id", user_controller.user_get);
 module.exports = router;
