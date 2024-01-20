7e37110f5d148a8a60953502f6b68030ed441181
fadelyking <fadely195@gmail.com>
2024-01-20 14:49:10 +0300

    small adjustment to verificaiton

--- middleware/verification.js
+++ middleware/verification.js
@@ -2,7 +2,6 @@ module.exports = function verifyToken(req, res, next) {
   const bearerHeader = req.cookies.token;
   if (typeof bearerHeader !== "undefined") {
     req.token = bearerHeader;
-
     next();
   } else {
     res.status(403).json("Invalid Token");
