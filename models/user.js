73b7a8361b5f58ab303392dce0c6a9b1169961a0
fadelyking <fadely195@gmail.com>
2024-01-20 16:18:56 +0300

    linked comments with user schema

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ const UserSchema = new Schema({
   email: String,
   password: String,
   admin: Boolean,
-  listings: { type: Schema.Types.ObjectId, ref: "Listing" },
+  listings: [{ type: Schema.Types.ObjectId, ref: "Listing" }],
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
