7983383ced856e6c3d7abcceed783072853e98cd
fadelyking <fadely195@gmail.com>
2024-01-21 09:53:16 +0300

    category controller

--- controllers/category.js
+++ controllers/category.js
@@ -1,2 +1,26 @@
 require("dotenv").config();
-const category = require("../models/category");
+const Categories = require("../models/category");
+
+exports.list_categories_get = asyncHandler(async (req, res, next) => {
+  const categories = await Categories.find().populate("listings").exec();
+
+  res.status(200).json({ success: true, categories: categories });
+});
+
+exports.category_detail_get = asyncHandler(async (req, res, next) => {
+  const category = await Categories.findOne(req.params.id).populate("listings").exec();
+
+  res.status(200).json({ success: true, category: category });
+});
+
+exports.category_add_post = asyncHandler(async (req, res, next) => {
+  const category = new Categories({
+    title: req.body.title,
+  });
+
+  await category.save();
+});
+
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  await Categories.findByIdAndDelete(req.body.id);
+});
