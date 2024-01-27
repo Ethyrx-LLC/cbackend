4be0b2b278624c5867c7add2bff48d2000d6aac9
fadelyking <fadely195@gmail.com>
2024-01-27 16:04:13 +0300

    populating only needed values for main page

--- controllers/listing.js
+++ controllers/listing.js
@@ -8,7 +8,10 @@ const User = require("../models/user");
 
 // Returns an array of listings
 exports.display_listings_all = asyncHandler(async (req, res, next) => {
-  const listings = await Listings.find().populate("user").populate("comments").exec();
+  const listings = await Listings.find()
+    .populate({ path: "user", select: "username emoji" })
+    .populate({ path: "comments", select: "user createdAt" })
+    .exec();
 
   res.status(200).json({ user: req.user, success: true, listings });
 });
