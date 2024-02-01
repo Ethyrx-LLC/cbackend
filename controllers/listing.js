f3d39746e32d4d6fefd6b58b06fcc74c3f8a1d9f
fadelyking <fadely195@gmail.com>
2024-02-01 14:27:14 +0300

    commented listing

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,3 +1,4 @@
+// Import necessary modules and models
 require("dotenv").config();
 const Listings = require("../models/listing");
 const asyncHandler = require("express-async-handler");
@@ -6,8 +7,9 @@ const Category = require("../models/category");
 const Comments = require("../models/comments");
 const User = require("../models/user");
 
-// Returns an array of listings
+// Returns an array of listings with populated user and comments data
 exports.display_listings_all = asyncHandler(async (req, res, next) => {
+  // Fetch listings with user and comments population
   const listings = await Listings.find()
     .populate({ path: "user", select: "username emoji" })
     .populate({
@@ -20,33 +22,42 @@ exports.display_listings_all = asyncHandler(async (req, res, next) => {
     })
     .exec();
 
+  // Respond with the populated listings
   res.status(200).json({ user: req.user, success: true, listings });
 });
 
-// Returns a specific listing based on ID
+// Returns details of a specific listing based on ID
 exports.display_listing_detail = asyncHandler(async (req, res, next) => {
+  // Fetch listing by ID with user and comments population
   const listing = await Listings.findById(req.params.id)
     .populate({ path: "user", select: "_id username emoji" })
     .populate("comments")
     .exec();
 
+  // Check if the listing exists
   if (listing === null) {
     res.status(404).json("Page not Found");
   } else {
+    // Increment views and save the updated listing
     listing.views += 1;
-
     await listing.save();
+    // Respond with the populated listing details
     res.status(200).json({ user: req.user, success: true, listing: listing });
   }
 });
 
+// Fetch categories for creating a new listing
 exports.create_listing_get = asyncHandler(async (req, res, next) => {
+  // Fetch categories
   const categories = Category.find().exec();
 
+  // Respond with categories
   res.status(200).json({ user: req.user, success: true, categories });
 });
 
+// Handle the creation of a new listing
 exports.create_listing_post = [
+  // Validation for the request body
   body("title", "Title must be more than 3 characters long").trim().isLength({ min: 3 }).escape(),
   body("content", "Content must be more than 10 letters long")
     .trim()
@@ -54,13 +65,18 @@ exports.create_listing_post = [
     .escape(),
   body("category", "Please select a category").notEmpty(),
 
+  // Process the creation of a new listing
   asyncHandler(async (req, res, next) => {
+    // Validate request body
     const errors = validationResult(req);
 
+    // Fetch the user who is creating the listing
     const poster = await User.findById(req.user._id).exec();
 
+    // Fetch the category based on the title provided in the request body
     const category = await Category.findOne({ title: req.body.category });
 
+    // Create a new listing object
     const listing = new Listings({
       user: req.user._id,
       title: req.body.title,
@@ -72,43 +88,53 @@ exports.create_listing_post = [
       createdAt: new Date(),
     });
 
-    // The line below would not accept category as string.
-    //const category = await Category.findById(req.body.category);
-
+    // Check for validation errors
     if (!errors.isEmpty()) {
       res.status(401).json({ success: false, error: errors.array() });
     } else {
+      // Save the new listing, update poster and category references
       await listing.save();
       poster.listings.push(listing);
       category.listings.push(listing);
       await category.save();
       await poster.save();
+      // Respond with success and the created listing
       res.status(200).json({ success: true, listing: listing });
     }
   }),
 ];
 
+// Handle the deletion of a listing
 exports.delete_listing_post = asyncHandler(async (req, res, next) => {
+  // Fetch the listing to be deleted
   const listing = Listings.findById(req.params.id).exec();
 
+  // Check if the listing exists
   if (listing === null) {
     res.json({ success: false, error: "No post found" });
   } else {
+    // Delete the listing and associated comments
     await listing.deleteOne();
     await Comments.deleteMany({
       listing: listing._id,
     });
+    // Respond with success
     res.json({ success: true, error: "Post deleted", authData });
   }
 });
 
+// Handle the upvoting of a listing
 exports.upvote_listing_post = asyncHandler(async (req, res, next) => {
+  // Fetch the listing and user
   const listing = await Listings.findById(req.params.id).exec();
   const user = await User.findById(req.user).exec();
+
+  // Check if the listing exists
   if (listing === null) {
     res.status(403).json({ success: false, error: "No post found" });
   }
 
+  // Check if the user has already upvoted the listing
   for (let listingLikes of user.listing_likes) {
     if (listingLikes._id.equals(listing._id)) {
       const found = user.listing_likes.findIndex((cm) => cm._id.equals(listing._id));
@@ -116,24 +142,33 @@ exports.upvote_listing_post = asyncHandler(async (req, res, next) => {
       listing.likes -= 1;
       await user.save();
       await listing.save();
+      // Respond with success and updated like information
       return res.json({ success: true, likes: listing.likes, user_likes: user.listing_likes });
     }
   }
+
+  // If user hasn't upvoted, add the upvote
   user.listing_likes.push(push);
   listing.likes += 1;
   await listing.save();
   await user.save();
+  // Respond with success and updated like information
   res.status(200).json({ success: true, likes: listing.likes, user_likes: user.listing_likes });
 });
 
+// Handle the increase in views for a listing
 exports.views_increase_listing_post = asyncHandler(async (req, res, next) => {
+  // Fetch the listing
   const listing = await Listings.findById(req.params.id).exec();
 
+  // Check if the listing exists
   if (listing === null) {
     res.status(403).json({ success: false, error: "No post found" });
   } else {
+    // Increment views and save the updated listing
     listing.views += 1;
     await listing.save();
+    // Respond with success and updated view count
     res.status(200).json({ success: true, likes: listing.likes });
   }
 });
