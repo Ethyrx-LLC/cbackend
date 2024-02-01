8dd091a8bae6a9d0eaa6dff2f37bef5cda71304b
fadelyking <fadely195@gmail.com>
2024-02-01 15:14:00 +0300

    commented app.js file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import necessary modules and packages
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
@@ -9,19 +10,33 @@ const session = require("express-session");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const passport = require("passport");
+
+// Import passport middleware configuration
 require("./middleware/passport")(passport);
+
+// Import routes
 const indexRouter = require("./routes/index");
+
+// Create an Express application
 const app = express();
+
+// Set up MongoDB connection
 const mongoDB = process.env.MONGO_URI;
 const MongoStore = require("connect-mongo");
+
 main().catch((err) => console.log(err));
+
 async function main() {
   await mongoose.connect(mongoDB);
 }
+
+// Configure middleware and settings
 app.use(
   cors({ credentials: true, origin: "http://localhost:3000", allowedHeaders: ["Content-Type"] })
 );
 app.use(cookieParser());
+
+// Configure and use session middleware with MongoDB as store
 app.use(
   session({
     secret: process.env.TOKEN_SECRET,
@@ -33,18 +48,30 @@ app.use(
     },
   })
 );
+
+// Initialize and use passport for authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Enable compression middleware
 app.use(compression());
+
+// Set up logger middleware
 app.use(logger("dev"));
+
+// Parse JSON requests
 app.use(bodyParser.json());
 
+// Set up static files and URL-encoded form data handling
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+
+// Use the routes defined in indexRouter
 app.use("/", indexRouter);
-const errorHandler = (error, req, res, bext) => {
+
+// Error handling middleware
+const errorHandler = (error, req, res, next) => {
   const errorMsg = {
     error_Message: error.message,
     error_Route: req.url,
@@ -57,8 +84,11 @@ const errorHandler = (error, req, res, bext) => {
 
   res
     .status(status)
-    .json("Internal error occured, our monkeys are hard at work to figure out what went wrong!");
+    .json("Internal error occurred, our monkeys are hard at work to figure out what went wrong!");
 };
+
+// Use the custom error handling middleware
 app.use(errorHandler);
 
+// Export the configured Express application
 module.exports = app;
