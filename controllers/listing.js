c2650d810017dc9c00bf0802c40e3d80c06c855a
fadelyking <fadely195@gmail.com>
2024-02-01 13:57:44 +0300

    removed last commenter from listing detail and schema

--- controllers/listing.js
+++ controllers/listing.js
@@ -9,11 +9,11 @@ const User = require("../models/user");
 // Returns an array of listings
 exports.display_listings_all = asyncHandler(async (req, res, next) => {
   const listings = await Listings.find()
-    .populate({ path: "user", select: "username emoji last_commenter" })
+    .populate({ path: "user", select: "username emoji" })
     .populate({ path: "comments", select: "user createdAt" })
     .exec();
 
-  res.status(200).json({ user: req.user, success: true, listings, last_commenter: req.lastPoster });
+  res.status(200).json({ user: req.user, success: true, listings });
 });
 
 // Returns a specific listing based on ID
