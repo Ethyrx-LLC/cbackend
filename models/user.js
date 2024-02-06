a53b6c95b51c5fbe69e8ab771cecbe2986aff43c
fadelyking <fadely195@gmail.com>
2024-02-06 15:27:59 +0300

    added perms and creation date to user schema

--- models/user.js
+++ models/user.js
@@ -6,11 +6,13 @@ const UserSchema = new Schema({
   email: String,
   password: String,
   emoji: String,
+  perms: Number,
   listing_likes: [{ type: Schema.Types.ObjectId, ref: "Listing" }],
   comment_likes: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
   comment_dislikes: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
   listings: [{ type: Schema.Types.ObjectId, ref: "Listing" }],
   comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("User", UserSchema);
