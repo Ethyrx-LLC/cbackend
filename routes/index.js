a0f0b07f1f6c9e82ed89d695a84bf8792de1df26
fadelyking <fadely195@gmail.com>
2024-02-07 12:48:22 +0300

    replaced var with const

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const listing_controller = require("../controllers/listing");
 const user_controller = require("../controllers/user");
 const comments_controller = require("../controllers/comments");
