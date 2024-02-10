5b98f07e3214701312fd37e7cfeb183eae9c57c9
fadelyking <fadely195@gmail.com>
2024-02-10 19:55:14 +0300

    fixed listing detail not saving because of lean()

--- controllers/listing.js
+++ controllers/listing.js
@@ -38,7 +38,6 @@ exports.display_listings_all = asyncHandler(async (req, res, next) => {
 exports.display_listing_detail = asyncHandler(async (req, res, next) => {
   // Fetch listing by ID with user and comments population
   const listing = await Listings.findById(req.params.id)
-    .lean()
     .populate({ path: "user", select: "_id username emoji createdAt" })
     .populate("comments")
     .exec();
