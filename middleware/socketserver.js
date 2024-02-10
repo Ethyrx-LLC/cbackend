a33ac1ad56e2054f701a0ee668dfcb6eda88ff9d
fadelyking <fadely195@gmail.com>
2024-02-10 14:16:46 +0300

    receiving current user id

--- middleware/socketserver.js
+++ middleware/socketserver.js
@@ -1,5 +1,5 @@
 const { Server } = require("socket.io");
-
+let onlineUsers = [];
 const initSocketServer = () => {
   const io = new Server(3001, {
     cors: ["http://localhost:5173"],
@@ -9,6 +9,10 @@ const initSocketServer = () => {
     console.log(socket.id);
     socket.on("online-status", (status) => {
       io.emit("online", status);
+    });
+
+    socket.on("new-user-add", (userID) => {
+      console.log(`CURRENT USER IS ${userID}`);
     });
   });
 
