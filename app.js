91a0787bf996e4c8b6be1764389088504b260b76
fadelyking <fadely195@gmail.com>
2024-02-14 16:25:41 +0300

    add rate limiter

--- app.js
+++ app.js
@@ -30,6 +30,19 @@ app.use(
   })
 );
 
+const compression = require("compression");
+const helmet = require("helmet");
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// …
 // Set up MongoDB connection
 const mongoDB = process.env.MONGO_URI;
 const MongoStore = require("connect-mongo");
