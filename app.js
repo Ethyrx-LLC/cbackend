be7ff680ccc387a5063908a1e453499389ba46b9
satanskitty <satanskitty@protonmail.ch>
2024-02-15 12:57:03 +0100

    set sameSite to none

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ const sessionMiddleware = session({
   cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       secure: true,
   },
 })
