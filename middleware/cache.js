273055117462cdd4fef50f2a376f7ccf0c9465be
fadelyking <fadely195@gmail.com>
2024-02-16 01:34:16 +0300

    changed to function

--- middleware/cache.js
+++ middleware/cache.js
@@ -1,4 +1,6 @@
 const apicache = require("apicache")
 let cache = apicache.middleware
-const cacheRoute = cache("5 minutes")
+function cacheRoute() {
+    cache("5 minutes")
+}
 module.exports = cacheRoute
