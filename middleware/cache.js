99ecfa1e7bf43e0cee346a16fb4b3bde04ba66cd
fadelyking <fadely195@gmail.com>
2024-02-16 12:16:54 +0300

    changed clearCache to function

--- middleware/cache.js
+++ middleware/cache.js
@@ -3,5 +3,8 @@ const cache = apicache.middleware
 
 const cacheRoute = cache("5 minutes")
 const cacheCookie = cache("30 seconds")
-const clearCache = apicache.clear
+const clearCache = (req, res, next) => {
+    apicache.clear()
+    next()
+}
 module.exports = { cacheCookie, cacheRoute, clearCache }
