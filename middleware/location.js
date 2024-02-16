c3ca20be32367dfa1415932a31648c17bbd8f9cd
fadelyking <fadely195@gmail.com>
2024-02-16 18:35:40 +0300

    another adjustment

--- middleware/location.js
+++ middleware/location.js
@@ -2,7 +2,7 @@ const { lookup } = require("geoip-lite")
 
 function findUserLocation(req, res, next) {
     console.log(lookup(req.ip))
-    console.log(lookup(req.headers["cf-connecting-ip"]))
+    console.log(lookup(socket.client.request.headers["cf-connecting-ip"]))
     next()
 }
 
