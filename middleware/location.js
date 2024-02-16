d93db43e7d4e72003a0a7d72e04cac6dc717ea39
fadelyking <fadely195@gmail.com>
2024-02-16 18:58:07 +0300

    reverted back to lookup

--- middleware/location.js
+++ middleware/location.js
@@ -1,14 +1,8 @@
 const { lookup } = require("geoip-lite")
-const ip = require("ip")
-function findUserLocation(req, res, next) {
-    const ip =
-        req.headers["cf-connecting-ip"] ||
-        req.headers["x-real-ip"] ||
-        req.headers["x-forwarded-for"] ||
-        req.socket.remoteAddress ||
-        ""
 
-    console.log(ip)
+function findUserLocation(req, res, next) {
+    console.log(lookup(req.ip))
+
     next()
 }
 
