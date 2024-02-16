0a5595d0601b4a4a5b9f779f232a92e04dfdcb1f
fadelyking <fadely195@gmail.com>
2024-02-16 11:35:41 +0300

    clear cache on logout

--- middleware/cache.js
+++ middleware/cache.js
@@ -2,5 +2,5 @@ const apicache = require("apicache")
 const cache = apicache.middleware
 
 const cacheRoute = cache("5 minutes")
-
-module.exports = cacheRoute
+const clearCache = apicache.clear
+module.exports = { cacheRoute, clearCache }
