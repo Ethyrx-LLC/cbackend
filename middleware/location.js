99ec0043fb120c53c5f7352cfd3604dd510b4f38
fadelyking <fadely195@gmail.com>
2024-02-16 18:32:55 +0300

    cloudfare connect ip

--- middleware/location.js
+++ middleware/location.js
@@ -2,6 +2,7 @@ const { lookup } = require("geoip-lite")
 
 function findUserLocation(req, res, next) {
     console.log(lookup(req.ip))
+    console.log(lookup(req.headers["cf-connecting-ip"]))
     next()
 }
 
