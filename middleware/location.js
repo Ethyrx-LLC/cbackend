4ed0c98000ba6249ab0b2bd83d6912b052a4fbe0
satanskitty <satanskitty@protonmail.ch>
2024-02-19 11:29:51 +0100

    formatting

--- middleware/location.js
+++ middleware/location.js
@@ -1,24 +1,24 @@
 const { lookup } = require("geoip-lite")
 
 function GeneratePublicIPv4() {
-    let firstOctet = Math.floor(Math.random() * 191) + 1;
+    let firstOctet = Math.floor(Math.random() * 191) + 1
     while ([10, 172, 192].includes(firstOctet)) {
-        firstOctet = Math.floor(Math.random() * 191) + 1;
+        firstOctet = Math.floor(Math.random() * 191) + 1
     }
-    const octets = [firstOctet];
+    const octets = [firstOctet]
     for (let i = 0; i < 3; i++) {
-        octets.push(Math.floor(Math.random() * 256));
+        octets.push(Math.floor(Math.random() * 256))
     }
-    const ipAddress = octets.join('.');
-    return ipAddress;
+    const ipAddress = octets.join(".")
+    return ipAddress
 }
 
 const env = process.env.NODE_ENV || "development"
 
-
 function findUserLocation(req, res, next) {
-    const userCountry = env === "development" ? lookup(GeneratePublicIPv4()) : lookup(req.ip);
-    console.log(userCountry);
+    const userCountry =
+        env === "development" ? lookup(GeneratePublicIPv4()) : lookup(req.ip)
+    console.log(userCountry)
     // Location can be undefined
     req.userLocation = {
         country: userCountry.country,
