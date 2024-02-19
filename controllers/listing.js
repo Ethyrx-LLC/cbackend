7c1a142be2c515a94cf915c5181d599d6f61a4ec
fadelyking <fadely195@gmail.com>
2024-02-19 03:38:38 +0300

    fixing location

--- controllers/listing.js
+++ controllers/listing.js
@@ -108,7 +108,7 @@ exports.create_listing_post = [
             res.status(401).json({ success: false, error: errors.array() })
         } else {
             // Save the new listing, update poster and category references
-
+            console.log(req.userLocation)
             await listing.save()
             poster.listings.push(listing)
             category.listings.push(listing)
