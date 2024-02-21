39d01a72246666bd5d57747a91f1596840ce938a
fadelyking <fadely195@gmail.com>
2024-02-21 14:29:28 +0300

    get all messages and send a message

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,9 +1,13 @@
 const User = require("../models/user")
-/* const Message = require("../models/messages") */
+const Message = require("../models/messages")
 const Chat = require("../models/chat")
 const asyncHandler = require("express-async-handler")
 
 // RECEIVE A MESSAGE
+exports.all_messages = asyncHandler(async (req, res) => {
+    const chat = Chat.findById(req.params.id).lean().exec()
+    res.status(200).json(chat.messages)
+})
 // SHOW ALL CONVERSATIONS
 exports.list_chats = asyncHandler(async (req, res) => {
     const chats = await Chat.find().exec()
@@ -24,3 +28,17 @@ exports.new_chat = asyncHandler(async (req, res) => {
     res.status(200).json({ success: true, chat })
 })
 // SEND A MESSAGE
+exports.send_message = asyncHandler(async (req, res) => {
+    const chat = Chat.findById(req.params.id).exec()
+    const sender = User.findById(req.user).exec()
+    const message = new Message({
+        sender: sender,
+        message: req.body.message,
+        createdAt: Date.now(),
+    })
+
+    chat.messages.push(message)
+    message.save()
+    chat.save()
+    res.status(200).json({ success: true, chat })
+})
