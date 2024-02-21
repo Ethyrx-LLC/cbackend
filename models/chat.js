f8e21140593b8c39cb35d349646fb2cc7010d70f
fadelyking <fadely195@gmail.com>
2024-02-21 20:12:33 +0300

    made messages required

--- models/chat.js
+++ models/chat.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema
 const ChatSchema = new Schema({
     sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
     receiver: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    messages: [{ type: Schema.Types.ObjectId, ref: "Messages" }],
+    messages: [
+        { type: Schema.Types.ObjectId, ref: "Messages", required: true },
+    ],
 })
 
 module.exports = mongoose.model("Chat", ChatSchema)
--- models/messages.js
+++ models/messages.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const MessageSchema = new Schema({
     sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    message: { type: String },
+    message: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 })
 
