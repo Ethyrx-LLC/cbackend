95a97eea73605e7c04d40ab7b5d71ebbd6a46d7e
fadelyking <fadely195@gmail.com>
2024-02-21 17:29:00 +0300

    populate chats

--- controllers/chat.js
+++ controllers/chat.js
@@ -10,7 +10,7 @@ exports.all_messages = asyncHandler(async (req, res) => {
 })
 // SHOW ALL CONVERSATIONS
 exports.list_chats = asyncHandler(async (req, res) => {
-    const userChats = await User.findById(req.user).exec()
+    const userChats = await User.findById(req.user).populate("chats").exec()
     res.status(200).json({ success: true, chats: userChats.chats })
 })
 
