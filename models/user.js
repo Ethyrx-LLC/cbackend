13bd17f4e802a545028f72341db10a5efe8f0e4f
fadelyking <fadely195@gmail.com>
2024-02-21 15:11:14 +0300

    typo in user schema

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema({
     emoji: String,
     role: Number,
     listing_likes: [{ type: Schema.Types.ObjectId, ref: "Listing" }],
-    chats: [{ type: Schema.Types.ObjectId, ref: "Chats" }],
+    chats: [{ type: Schema.Types.ObjectId, ref: "Chat" }],
     comment_likes: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
     comment_dislikes: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
     listings: [{ type: Schema.Types.ObjectId, ref: "Listing" }],
