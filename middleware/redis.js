860a6fd5b7cc8066a96fae5fac6c4a989e35d3a7
fadelyking <fadely195@gmail.com>
2024-02-24 12:41:05 +0300

    tweaked redis middleware

--- middleware/redis.js
+++ middleware/redis.js
@@ -1,14 +1,15 @@
 const redis = require("redis")
 let redisClient
+async function RunRedis() {
+    redisClient = redis.createClient()
 
+    redisClient.on("error", (error) => console.error(`Error : ${error}`))
+
+    await redisClient.connect()
+}
+
+RunRedis()
 function verifyListingsCache(parameter) {
-    ;(async () => {
-        redisClient = redis.createClient()
-
-        redisClient.on("error", (error) => console.error(`Error : ${error}`))
-
-        await redisClient.connect()
-    })()
     return async (req, res, next) => {
         const cache = parameter
 
@@ -17,7 +18,6 @@ function verifyListingsCache(parameter) {
             const listings = JSON.parse(cacheResults)
             console.log(listings.length)
             if (cacheResults) {
-                isCached = true
                 return res.status(200).json({
                     listings,
                 })
@@ -33,51 +33,13 @@ function verifyListingsCache(parameter) {
 }
 
 function verifyListingDetailCache(parameter) {
-    ;(async () => {
-        redisClient = redis.createClient()
-
-        redisClient.on("error", (error) => console.error(`Error : ${error}`))
-
-        await redisClient.connect()
-    })()
     return async (req, res, next) => {
         const cache = parameter
 
         try {
             const cacheResults = await redisClient.get(cache)
             if (cacheResults) {
-                listing = JSON.parse(cacheResults)
-                res.status(200).json({
-                    listing,
-                })
-            } else {
-                next()
-            }
-        } catch (error) {
-            console.error(error)
-            res.status(404)
-            next()
-        }
-    }
-}
-
-function verifyListingDetailCache(parameter) {
-    ;(async () => {
-        redisClient = redis.createClient()
-
-        redisClient.on("error", (error) => console.error(`Error : ${error}`))
-
-        await redisClient.connect()
-    })()
-    return async (req, res, next) => {
-        const cache = parameter
-
-        try {
-            const cacheResults = await redisClient.get(cache)
-            if (cacheResults) {
-                isCached = true
-                results = JSON.parse(cacheResults)
-
+                const listing = JSON.parse(cacheResults)
                 res.status(200).json({
                     listing,
                 })
