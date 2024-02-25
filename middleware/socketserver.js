0fdf2c963f1dee755267575555ad1d92b9093223
satanskitty <satanskitty@protonmail.ch>
2024-02-25 10:17:09 +0100

    test push

--- middleware/socketserver.js
+++ middleware/socketserver.js
@@ -2,7 +2,7 @@ const { Server } = require("socket.io")
 
 // Array to store information about online users
 let onlineUsers = []
-const sockets = {}
+//const sockets = {}
 
 // Function to initialize the Socket.IO server
 const initSocketServer = () => {
@@ -61,31 +61,35 @@ const initSocketServer = () => {
             io.emit("get-users", onlineUsers)
         })
 
-        socket.on("send-message", (data) => {
-            if (data.userId === undefined) {
-                return console.log("receiver is offline")
-            }
-            const isReceiverOnline = onlineUsers.find(
-                (receiver) => receiver.userId === data.receiver
-            )
+        // socket.on("send-message", (data) => {
+        //     // if (data.userId === undefined) {
+        //     //     return console.log("receiver is offline")
+        //     // }
+        //     console.log(data);
+        //     const isReceiverOnline = onlineUsers.find(
+        //         (receiver) => receiver.userId === data.receiver
+        //     )
+        //     const isSenderOnline = onlineUsers.find(
+        //         (sender) => sender.userId === data.sender
+        //     )
 
-            if (isReceiverOnline !== undefined) {
-                console.log(data.message)
-                io.to(isReceiverOnline.socketId).emit("message-received", {
-                    chatId: data.chatId,
-                    message: data.message,
-                })
-            }
+        //     if (isReceiverOnline !== undefined) {
+        //         console.log(data.message)
+        //         io.to(isReceiverOnline.socketId).emit("message-received", {
+        //             chatId: data.chatId,
+        //             message: data.message,
+        //         })
+        //     }
 
-            if (isSenderOnline !== undefined) {
-                console.log(data.message)
-                io.to(isSenderOnline.socketId).emit("message-received", {
-                    chatId: data.chatId,
-                    message: data.message,
-                })
-            }
-        })
-        /*    socket.on(
+        //     if (isSenderOnline !== undefined) {
+        //         console.log(data.message)
+        //         io.to(isSenderOnline.socketId).emit("message-received", {
+        //             chatId: data.chatId,
+        //             message: data.message,
+        //         })
+        //     }
+        // })
+         socket.on(
             "send-message",
             (chatId, poster, sender, receiver, message) => {
                 // Since we have a sender and receiver which are constant on the database, we have to kind of reverse the
@@ -130,7 +134,7 @@ const initSocketServer = () => {
                     )
                 }
             }
-        ) */
+        ) 
     })
 
     // Return the configured Socket.IO server instance
