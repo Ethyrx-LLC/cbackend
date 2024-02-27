f07a7ef5333a4e12804c4faf218ebd915d520be1
satanskitty <satanskitty@protonmail.ch>
2024-02-27 10:09:18 +0100

    remove hint

--- controllers/listing.js
+++ controllers/listing.js
@@ -28,7 +28,6 @@ exports.display_listings_all = asyncHandler(async (req, res) => {
         .skip(startIndex)
         .limit(limit)
         .sort({ createdAt: -1 })
-        .hint({ createdAt: -1 })
         .lean()
         .populate({ path: "user", select: "username emoji" })
         .populate({
