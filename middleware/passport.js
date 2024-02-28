1cadf403f90fe89a23365f318df7d7f2fbec6be9
fadelyking <fadely195@gmail.com>
2024-02-28 11:40:45 +0300

    fixed missing number in module

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const passport = require("passport")
 const LocalStrategy = require("passport-local").Strategy
-const GoogleStrategy = require("passport-google-oauth2").Strategy
+const GoogleStrategy = require("passport-google-oauth20").Strategy
 const User = require("../models/user")
 const bcrypt = require("bcryptjs")
 
