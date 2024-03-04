226f57c28417b5141d4bbe68d63fea245a6fcb64
fadelyking <fadely195@gmail.com>
2024-03-04 12:21:14 +0300

    imported asynchandler and user model to permissions to improve permissions

--- middleware/permissions.js
+++ middleware/permissions.js
@@ -1,9 +1,11 @@
+/* require("dotenv").config()
+const asyncHandler = require("express-async-handler")
+const User = require("../models/user") */
 const ROLE = {
     ADMIN: 100,
     MOD: 50,
     BASIC: 0,
 }
-
 function AuthorizeDashboard(req, res, next) {
     if (req.user.perms !== ROLE.ADMIN && req.user.perms !== ROLE.MOD) {
         res.status(401).json("You are not authorized")
