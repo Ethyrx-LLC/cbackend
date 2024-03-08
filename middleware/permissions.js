67f472d8652c2f14dfb71f6844d3eba7b6c41280
fadelyking <fadely195@gmail.com>
2024-03-08 03:32:38 +0300

    better authorization/roles

--- middleware/permissions.js
+++ middleware/permissions.js
@@ -1,19 +1,38 @@
-/* require("dotenv").config()
+require("dotenv").config()
 const asyncHandler = require("express-async-handler")
-const User = require("../models/user") */
+const Listing = require("../models/listing")
 const ROLE = {
     ADMIN: 100,
     MOD: 50,
     BASIC: 0,
 }
-function AuthorizeDashboard(req, res, next) {
-    if (req.user.perms !== ROLE.ADMIN && req.user.perms !== ROLE.MOD) {
+
+exports.DELETE_LISTING = asyncHandler(async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id).exec()
+    if (
+        listing.user !== req.user &&
+        req.user.perms !== ROLE.MOD &&
+        req.user.perms !== ROLE.ADMIN
+    ) {
+        res.status(401).json("You are not authorized")
+    } else {
+        res.status(200).json("You are authorized")
+        next()
+    }
+})
+
+exports.MOD = asyncHandler(async (req, res) => {
+    if (req.user.perms !== ROLE.MOD) {
         res.status(401).json("You are not authorized")
     }
-    next()
-}
+})
+
+exports.ADMIN = asyncHandler(async (req, res) => {
+    if (req.user.perms !== ROLE.ADMIN) {
+        res.status(401).json("You are not authorized")
+    }
+})
 
 module.exports = {
     ROLE,
-    AuthorizeDashboard,
 }
