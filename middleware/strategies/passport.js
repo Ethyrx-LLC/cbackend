cb736784a732d10d8215f817df9b863481bb1bd0
fadelyking <fadely195@gmail.com>
2024-03-16 00:29:46 +0300

    removed console log

--- middleware/strategies/passport.js
+++ middleware/strategies/passport.js
@@ -8,7 +8,6 @@ module.exports = function (passport) {
     passport.use(
         new LocalStrategy(async (username, password, done) => {
             try {
-                console.log(username)
                 const user = await User.findOne({
                     $or: [{ username: username }, { email: username }],
                 })
