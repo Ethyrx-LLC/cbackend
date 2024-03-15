0a8d5c68836f959d2a4e802afc429e847a671caf
fadelyking <fadely195@gmail.com>
2024-03-16 00:12:13 +0300

    meta description to category schema

--- models/category.js
+++ models/category.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const CategorySchema = new Schema({
     title: String,
     listings: [{ type: Schema.Types.ObjectId, ref: "Listing" }],
+    meta_description: String,
 })
 
 module.exports = mongoose.model("Category", CategorySchema)
